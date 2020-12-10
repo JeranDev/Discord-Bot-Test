386494d3b8e719dad5271cb2cb147ea438c12d9f
JeranDev <jeranjb@gmail.com>
2020-12-10 11:51:58 -0700

    add chewey API

--- bot.js
+++ bot.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
 
 const Discord = require('discord.js')
+const axios = require('axios');
 const client = new Discord.Client({
   partials: ['MESSAGE'],
 })
 
+const cheweyURL = 'https://api.chewey-bot.top/'
+
 const BOT_PREFIX = '!'
-const MOD_ME_COMMAND = 'mod-me'
 
 client.on('ready', () => {
-  console.log('Our bot is ready to go!!!')
+  console.log('Our bot is ready to go')
 })
 
 client.on('messageDelete', msg => {
@@ -17,16 +19,28 @@ client.on('messageDelete', msg => {
 })
 
 client.on('message', msg => {
-  if (msg.content === 'based') {
+  if (msg.content.split(/[\s,\?\,\.!]+/).some(word => word === 'based')) {
+    msg.react(`🇧`)
+    msg.react(`🇦`)
+    msg.react(`🇸`)
+    msg.react(`🇪`)
+    msg.react(`🇩`)
     msg.react(`😍`)
   }
-  if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
-    modUser(msg.member)
+  if (msg.content.startsWith(`${BOT_PREFIX}`)) {
+    const messageRegex = msg.content.replace(/[\s,\?\,\.!]+/, '')
+    console.log(messageRegex)
+    cheweyFunction(msg, messageRegex)
   }
 })
 
-function modUser(member) {
-  member.roles.add('785909730677751898')
+async function cheweyFunction(msg, command) {
+  try {
+    const response = await axios.get(`${cheweyURL}${command}${process.env.CHEWEY_API_KEY}`)
+    msg.channel.send(`${response.data.data}`)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 client.login(process.env.BOT_TOKEN)
